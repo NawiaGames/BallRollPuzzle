c8c4ef230a9cb6b6a705e341bba1d39eab233d6d
ngmroz <71435149+ngmroz@users.noreply.github.com>
2022-06-13 13:27:33 +0200

    -

--- Assets/_yab/Scripts/Game/Level.cs
+++ Assets/_yab/Scripts/Game/Level.cs
@@ -795,15 +795,18 @@ public class Level : MonoBehaviour
           else
           {
             var ci = _grid.geti(_pushing[p].grid);
-            if(ci && ci.IsDirectional && _pushing[p].Redirected != null)
+            if(ci && ci.IsDirectional)
             {
-              _pushing[p].Redirected = ci.vturn;
-              _pushing[p].Stop();
-              _pushing[p].dir = ci.vturn;
-              _pushing[p].MoveP(Time.deltaTime * _speed * 5);
-              //_items.Add(_pushing[p]);
-              // _pushing.RemoveAt(p);
-              // p--;
+              if(_pushing[p].Redirected == null)
+              {  
+                _pushing[p].Redirected = ci.vturn;
+                _pushing[p].Stop();
+                _pushing[p].dir = ci.vturn;
+              }
+            }
+            else
+            {
+              _pushing[p].Redirected = null;
             }
           }
         }
