00f2bf906eff8ae126428598293dafaba2d4e49a
msuchy82 <theunspokenterror@gmail.com>
2022-06-21 01:35:03 +0200

    water texture tweaks, minor ui changes (tutorial)

--- Assets/_yab/Scripts/Game/UI/UITutPwrInfo.cs
+++ Assets/_yab/Scripts/Game/UI/UITutPwrInfo.cs
@@ -6,17 +6,14 @@ using GameLib.UI;
 public class UITutPwrInfo : MonoBehaviour
 {
   [SerializeField] UIPanel panel;
-  [SerializeField] UIPanel btnPanel;
   
   public void Activate() 
   {
     panel.ActivatePanel(); 
-    btnPanel.ActivatePanel();
   }
   public void Deactivate() 
   {
     panel.DeactivatePanel();
-    //btnPanel.DeactivatePanel();
   }
 
   public void Hide()
