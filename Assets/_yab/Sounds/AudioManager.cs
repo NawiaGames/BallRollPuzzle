c0605167f51b09043012fadfa66cb6f5518fff0a
msuchy82 <theunspokenterror@gmail.com>
2022-06-24 10:07:33 +0200

    awesome sounds pass 2 (please change)

--- Assets/_yab/Sounds/AudioManager.cs
+++ Assets/_yab/Sounds/AudioManager.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 using GameLib.AudioSystem;
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] AudioPlayer ballSpawnAudio = null;
     [SerializeField] AudioPlayer ballHitAudio = null;
+    [SerializeField] AudioPlayer ballDestroyAudio = null;
+    [SerializeField] AudioPlayer victoryAudio = null;
 
     private void OnEnable() {
         Item.onHit += PlayBallHitSFX;
+        Item.onPushedOut += PlayBallDestroyAudio;
+        Level.onDone += PlayConfettiAudio;
     }
     private void OnDisable() {
         Item.onHit -= PlayBallHitSFX;        
+        Item.onPushedOut -= PlayBallDestroyAudio;
+        Level.onDone -= PlayConfettiAudio;
     }
 
+    void PlayBallSpawnSFX(object sender) => ballSpawnAudio.Play();
     void PlayBallHitSFX(object sender) => ballHitAudio.Play();
+    void PlayBallDestroyAudio(object sender) => ballDestroyAudio.Play();
+    void PlayConfettiAudio(object sender) => victoryAudio.Play();
+
 }
